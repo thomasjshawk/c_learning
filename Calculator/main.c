bd89f07cce215890ea3256f23709faa6174a0001
showquey <thomas.j.shawk@gmail.com>
2025-01-11 13:26:49 -0500

    Initial Commit

---
+++ Calculator/main.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdio.h>
+
+char operation();
+double add(double num1, double num2);
+double subtract(double num1, double num2);
+double multiply(double num1, double num2);
+double divide(double num1, double num2);
+
+double main()
+{
+	double fnum, snum;
+	char oper;
+
+	printf("Welcome to the calculator app!\n");
+
+	oper = operation();
+	
+	printf("Please enter the first number: ");
+	scanf("%lf", &fnum);
+
+	printf("Please enter the second number: ");
+	scanf("%lf", &snum);
+
+	if(oper == '+'){
+		printf("%lf\n", add(fnum, snum));
+	} else if(oper == '-'){
+		printf("%lf\n", subtract(fnum, snum));
+	} else if(oper == '*'){
+		printf("%lf\n", multiply(fnum, snum));
+	} else{
+		printf("%lf\n", divide(fnum, snum));
+	}
+	
+
+	return 0;
+}
+
+char operation(){
+	char operator;
+	printf("Please enter the desired operation: ");
+	scanf(" %c", &operator);
+
+	return operator;
+
+}
+
+double add(double num1, double num2){
+	double result;
+
+	result = num1 + num2;
+	return result;
+}
+
+double subtract(double num1, double num2){
+	double result;
+
+	result = num1 - num2;
+	return result;
+}
+
+double multiply(double num1, double num2){
+	double result;
+
+	result = num1 * num2;
+	return result;
+}
+
+double divide(double num1, double num2){
+	double result;
+
+	result = num1 / num2;
+	return result;
+}
