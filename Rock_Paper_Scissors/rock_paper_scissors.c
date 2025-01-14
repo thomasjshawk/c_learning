e4e4f3e778f51744f27025bacc2b940db90cf4ed
showquey <thomas.j.shawk@gmail.com>
2025-01-13 22:37:38 -0500

    Add necessary variables for RPS game

---
+++ Rock_Paper_Scissors/rock_paper_scissors.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+	unsigned int seed = time(0);
+	int rd_num;
+	int p_score = 0;
+	int c_score = 0;
+	const int w_score = 2;
+
+
+
+	return 0;
+}
+
