c8c858ce8904d2f80b63df3d12f1a32a6e80532d
showquey <thomas.j.shawk@gmail.com>
2025-01-17 21:28:26 -0500

    Finish rock papers scissors game, add comments that should have been added during the process

--- Rock_Paper_Scissors/rock_paper_scissors.c
+++ Rock_Paper_Scissors/rock_paper_scissors.c
@@ -14,23 +14,85 @@ int main(){
 
 	printf("This is rock paper scissors! Best two out of 3 wins!\n");
 
-	while (p_score < 2 && c_score < 2) {
+	while (p_score < 2 && c_score < 2) {  /* While loop checks for game winning conditions */ 
 		
-		c_choice = rand_r(&seed) % 3 + 1;
+		c_choice = rand_r(&seed) % 3 + 1; /* Makes random choice for computer */
 
-		while (p_choice != 1 && p_choice != 2 && p_choice != 3) {
+		do {
 			p_choice = choose();
+		} while (p_choice != 1 && p_choice != 2 && p_choice != 3);
+		
+		switch (c_choice) {  /* Prints out what the computer chose */
+			case 1:
+				printf("Computer chose rock!\n");
+				break;
+			case 2:
+				printf("Computer chose paper!\n");
+				break;
+			case 3:
+				printf("Computer chose scissors!\n");
+				break;
 		}
 
+		if (p_choice == 1) { /* Activates switch that corresponds to player choice and responding win or loss for computer */
+			switch (c_choice) {
+				case 1:
+					printf("\nTie!\n\n");
+					break;
+				case 2:
+					printf("\n1 point for computer!\n\n");
+					c_score++;
+					break;
+				case 3:
+					printf("\n1 point for you!\n\n");
+					p_score++;
+					break;
+			}
+		} else if (p_choice == 2) {
+			switch (c_choice) {
+				case 1:
+					printf("\n1 point for you!\n\n");
+					p_score++;
+					break;
+				case 2:
+					printf("\nTie!\n\n");
+					break;
+				case 3:
+					printf("\n1 point for computer!\n\n");
+					c_score++;
+					break;
+			}
+		} else {
+			switch (c_choice) {
+				case 1:
+					printf("\n1 point for computer!\n\n");
+					c_score++;
+					break;
+				case 2:
+					printf("\n1 point for you!\n\n");
+					p_score++;
+					break;
+				case 3:
+					printf("\nTie!\n\n");
+					break;
+			}
+		}
 	}
+
+	if (p_score == 2) { /* Prints out if you won or lost the game */
+		printf("\nYOU WIN!\n");
+	} else {
+		printf("\nYOU LOSE!\n");
+	}
+	
 	return 0;
 }
 
-int choose(){
+int choose(){ /* Prompts user for choice of 'weapon' and returns choice */
 	int choice;	
 	printf("Please enter 1 for Rock, 2 for Paper, 3 for Scissors: ");
 	scanf("%d", &choice);
 	return choice;
-}
+} 
 
 	
