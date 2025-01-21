10c6bad0f7e6cf500e8c6a0c0d35405d4f1beee0
showquey <thomas.j.shawk@gmail.com>
2025-01-20 23:18:59 -0500

    Add information for user when opening application, create numbering system for to-do items

--- To_Do_List/to_do_list.c
+++ To_Do_List/to_do_list.c
@@ -4,25 +4,32 @@
 
 int main(){
 	FILE *tlist;
-	char *list = "list.txt";
+	char *filename = "list.txt";
 	char line[1000];
+	int filelength = 0;
 
 	// Checks for existing list.txt, if nonexistent creates it	
-	if (access(list, F_OK) == -1) {
+	if (access(filename, F_OK) == -1) {
 		tlist = fopen("list.txt", "w");
 
 		fclose(tlist);
+	} else {
+		printf("You have the following items on your to-do list\n");
 	}
 
-	// Print existing lines in list.txt
+	// Print existing lines in list.txt, and number them
 	tlist = fopen("list.txt", "r");
 
 	while (fgets(line, 1000, tlist)) {
-		printf("%s", line);
+		filelength++;
+		printf("%d. %s", filelength, line);
 	}
 
 	fclose(tlist);
 
+	// Allows user to append item to list and numbers them
+	
+	
 	
 
 
