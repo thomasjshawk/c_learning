c52a7629be63532343983e4da5ec6af01df4ec4b
showquey <thomas.j.shawk@gmail.com>
2025-01-21 22:41:32 -0500

    Turn two primary actions into functions

--- To_Do_List/to_do_list.c
+++ To_Do_List/to_do_list.c
@@ -2,30 +2,19 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+void create_file(char *fname, FILE *list);
+void print_number(FILE *list, char sline[], int filelen);
+
 int main(){
 	FILE *tlist;
 	char *filename = "list.txt";
 	char line[1000];
 	int filelength = 0;
 
-	// Checks for existing list.txt, if nonexistent creates it	
-	if (access(filename, F_OK) == -1) {
-		tlist = fopen("list.txt", "w");
-
-		fclose(tlist);
-	} else {
-		printf("You have the following items on your to-do list\n");
-	}
-
+	create_file(filename, tlist);
+	
 	// Print existing lines in list.txt, and number them
-	tlist = fopen("list.txt", "r");
-
-	while (fgets(line, 1000, tlist)) {
-		filelength++;
-		printf("%d. %s", filelength, line);
-	}
-
-	fclose(tlist);
+	print_number(tlist, line, filelength);
 
 	// Allows user to append item to list and numbers them
 	
@@ -37,3 +26,29 @@ int main(){
 
 	return 0;
 }
+
+// Accepts file name and file pointer, checks if file exists, if it doesn't create file, if it does print header
+void create_file(char *fname, FILE *list) {
+	if (access(fname, F_OK) == -1) {
+		list = fopen("list.txt", "w");
+
+		fclose(list);
+	} else {
+		printf("You have the following items on your to-do list\n");
+	}
+}
+
+// Prints each line of existing list.txt file
+void print_number(FILE *list, char sline[], int filelen) {
+	list = fopen("list.txt", "r");
+
+	while (fgets(sline, 1000, list)) {
+		filelen++;
+		printf("%d. %s", filelen, sline);
+	}
+
+	fclose(list);
+}
+
+
+
